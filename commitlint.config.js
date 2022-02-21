81e1ddb78b076acbaf7314b8a8e7f84c03e504a5
Xiaoqi <2658407665@qq.com>
2022-02-21 16:45:39 +0800

    docs: 变更README

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,14 +1,7 @@
 ﻿module.exports = {
   extends: ["cz"],
   rules: {
-    // "type-empty": [2, "never"],
-    "type-case": [0],
-    "type-empty": [0],
-    "scope-empty": [0],
-    "scope-case": [0],
-    "subject-full-stop": [0, "never"],
-    "subject-case": [0, "never"],
-    "header-max-length": [0, "always", 72],
+    "type-empty": [2, "never"],
     "type-enum": [
       2,
       "always",
