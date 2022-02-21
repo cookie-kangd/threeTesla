e45d93b3811c9d3df70090c67f270e9479dcd343
Xiaoqi <2658407665@qq.com>
2022-02-21 10:32:17 +0800

    特性: 规范commit

--- .cz-config.js
+++ .cz-config.js
@@ -1,23 +1,25 @@
+"use strict";
+
 module.exports = {
   types: [
-    { value: "feat", name: "feat: 一个新的特性" },
-    { value: "fix", name: "fix: 修复一个Bug" },
-    { value: "docs", name: "docs: 变更的只有文档" },
-    { value: "style", name: "style: 代码风格,格式修复" },
-    { value: "refactor", name: "refactor: 代码重构，注意和feat、fix区分开" },
-    { value: "perf", name: "perf: 码优化,改善性能" },
-    { value: "test", name: "test: 测试" },
-    { alue: "chore", name: "chore: 变更构建流程或辅助工具" },
-    { value: "revert", name: "revert: 代码回退" },
-    { value: "init", name: "init: 项目初始化" },
-    { value: "build", name: "build: 变更项目构建或外部依赖" },
-    { value: "WIP", name: "WIP: 进行中的工作" },
+    { value: "特性", name: "特性:    一个新的特性" },
+    { value: "修复", name: "修复:    修复一个Bug" },
+    { value: "文档", name: "文档:    变更的只有文档" },
+    { value: "格式", name: "格式:    空格, 分号等格式修复" },
+    { value: "重构", name: "重构:    代码重构，注意和特性、修复区分开" },
+    { value: "性能", name: "性能:    提升性能" },
+    { value: "测试", name: "测试:    添加一个测试" },
+    { value: "工具", name: "工具:    开发工具变动(构建、脚手架工具等)" },
+    { value: "回滚", name: "回滚:    代码回退" },
   ],
-  scopes: [],
-  allowTicketNumber: false,
-  isTicketNumberRequired: false,
-  ticketNumberPrefix: "TICKET-",
-  ticketNumberRegExp: "\\d{1,5}",
+
+  scopes: [
+    { name: "模块1" },
+    { name: "模块2" },
+    { name: "模块3" },
+    { name: "模块4" },
+  ],
+
   // it needs to match the value for field type. Eg.: 'fix'
   /*
   scopeOverrides: {
@@ -35,19 +37,16 @@ module.exports = {
     scope: "选择一个scope (可选):",
     // used if allowCustomScopes is true
     customScope: "Denote the SCOPE of this change:",
-    subject: "简短说明(最多40个字):",
+    subject: "短说明:\n",
     body: '长说明，使用"|"换行(可选)：\n',
     breaking: "非兼容性说明 (可选):\n",
-    footer: "关联关闭的issue，例如：#12, #34(可选):\n",
-    confirmCommit: "确定提交?",
+    footer: "关联关闭的issue，例如：#31, #34(可选):\n",
+    confirmCommit: "确定提交说明?",
   },
+
   allowCustomScopes: true,
-  allowBreakingChanges: ["feat", "fix"],
-  // skip any questions you want
-  skipQuestions: ["scope", "body", "breaking"],
+  allowBreakingChanges: ["特性", "修复"],
+
   // limit subject length
   subjectLimit: 100,
-  // breaklineChar: '|', // It is supported for fields body and footer.
-  // footerPrefix : 'ISSUES CLOSED:'
-  // askForBreakingChangeFirst : true, // default is false
 };
